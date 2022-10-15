e8738a76471eddeb4c9acf635137efb14ce8314f
mngunawardana <mngunawardana.students.nsbm.ac.lk>
2022-10-15 22:46:26 +0530

    added new for loop

--- struct.c
+++ struct.c
@@ -1,5 +1,10 @@
 #include "stdio.h"
 int main(){
     printf("Hello Wasath");
+    printf("This is second line in c\n");
+
+    for (int i = 0; i < 10; ++i) {
+        printf("Lithara\n");
+    }
 
 }
