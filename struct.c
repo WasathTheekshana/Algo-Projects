c8faef77a0a7cc45d90247d4cdfd8203b50d7948
WasathTheekshana <wasath.vt@gmail.com>
2022-10-18 11:42:44 +0530

    stact data structure

--- struct.c
+++ struct.c
@@ -1,12 +1,27 @@
-#include "stdio.h"
-int main(){
-    printf("Hello Wasath");
-    printf("This is second line in c\n");
+#include<stdio.h>
 
-    for (int i = 0; i < 10; ++i) {
-        printf("Lithara\n");
-        printf("Wasath\n");
-        printf("hi lithara");
-    }
+struct myStack{
+    int myarray[5];
+    int top; //error 1
+}st;
+
+void push(int ele){ //error 2
+    st.top++;
+    st.myarray[st.top]=ele; //error 3
+    printf("Item Added to index %d\n", st.top);
+}
+
+int pop(int ele){ //error 4
+    int out=st.myarray[st.top]; //error 5
+    printf("popped out item at index %d", st.top);	//error 6
+}
+
+int main(){	//error 7
+     push(10);
+     push(20);
+     push(30);
+     pop(25);
+    return 0;
 
 }
+
